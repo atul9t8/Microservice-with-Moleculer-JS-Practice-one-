610663dd8024ba5f8b06526d2cd09988046ff316
atul9t8 <atuldebnath8@yahoo.com>
2024-11-30 16:56:50 +0600

    email service added to index file.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const UserService = require("./services/userService");
+const EmailService = require("./services/emailService");
 
 async function startApp() {
   await UserService.start();
+  await EmailService.start();
   try {
     const newUser = await UserService.call("user.createUser", {
       username: "Jhon",
@@ -10,10 +12,19 @@ async function startApp() {
     console.log("New User", newUser);
     const getAllUser = await UserService.call("user.getUser");
     console.log("All User", getAllUser);
+
+    //Email Service
+    const sendingEmail = await EmailService.call("email.sendEmail", {
+      emailID: newUser.email,
+      subject: "This is the main subject",
+      content: "Thank you!",
+    });
+    console.log(sendingEmail);
   } catch (err) {
     console.log("Error:", err);
   } finally {
     UserService.stop();
+    EmailService.stop();
   }
 }
 
